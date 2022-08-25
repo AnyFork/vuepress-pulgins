01b0d4ea3a78baa34a73bb07a67373af2b07144a
anyfork <874957743@qq.com>
2022-08-25 10:00:07 +0800

    🐞 fix: 修复趣味动态标题全局组件注册问题

--- packages/@anyfork/vuepress-plugin-dynamic-title-next/src/client/config.ts
+++ packages/@anyfork/vuepress-plugin-dynamic-title-next/src/client/config.ts
@@ -4,5 +4,6 @@ import DynamicTitle from './components/DynamicTitle.vue'
 export default defineClientConfig({
   enhance({ app }) {
     app.component(DynamicTitle.name, DynamicTitle)
-  }
+  },
+  rootComponents: [DynamicTitle]
 })
